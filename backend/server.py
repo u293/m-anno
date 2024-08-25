3b71897bd916e0ddf6fa2e99ba1580fc4ca7cdcb
f@Latitude-5590 <f@Latitude-5590>
2024-08-25 14:36:03 +0200

    same v2 commit

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_cors import CORS
+import numpy as np
 import json
 import os
 import re
@@ -9,32 +10,56 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the Quranic verses from the Excel file
-df_verses = pd.read_excel('Dataset-Verse-by-Verse.xlsx')
+# df_verses = pd.read_excel('Dataset-Verse-by-Verse.xlsx')
+# if 'AyahKey' not in df_verses.columns:
+#     df_verses['AyahKey'] = df_verses['SurahNo'].astype(str) + ":" + df_verses['AyahNo'].astype(str)
+df_verses = pd.read_excel('warshData_v2-1.xlsx', dtype=str)
+#id	jozz	page	sura_no	sura_name_en	sura_name_ar	line_start	line_end	aya_no	aya_text
+if 'AyahKey' not in df_verses.columns:
+    df_verses['AyahKey'] = df_verses['sura_no'].astype(str) + ":" + df_verses['aya_no'].astype(str)
 
 # Load annotations from the CSV files
+# manus_id_name_mapper = {0: "Konduga"}
+
 annotations = {}
-for i in range(1, 14):
-    csv_file = f'annotations_{i}.csv'
-    if os.path.exists(csv_file):
-        annotations[i] = pd.read_csv(csv_file).to_dict(orient='records')
+annotations["Konduga"] = pd.read_excel("Dataset-Verse-by-Verse1-Konduga.xlsx").astype(str)
+annotations["Konduga"] = annotations["Konduga"].replace('nan','',regex=True)
+if "annotation_id" not in annotations["Konduga"].columns:
+    annotations["Konduga"].insert(0, 'annotation_id', range(0, len(annotations["Konduga"])))
+annotations["Konduga"] = annotations["Konduga"].rename(columns={"annotation_aya": "verse_id",
+                                                                "annotation_Konduga": "annotation",
+                                                                "annotation_transliteration": "annotation_transliteration",
+                                                                })
+# print(annotations[0].columns)
+
+# print(annotations["Konduga"])
+
+
+# for i in range(1, 2):
+#     csv_file = f'annotations_{i}.csv'
+#     if os.path.exists(csv_file):
+#         annotations[i] = pd.read_csv(csv_file).to_dict(orient='records')
 
 def starts_with_arabic(text):
     arabic_pattern = re.compile(r'^[\u0600-\u06FF\u0750-\u077F]')
     return bool(arabic_pattern.match(text))
+
 
 @app.route('/search_verse', methods=['GET'])
 def search():
     query = request.args.get('query', '')
     if query[0].isdigit():
         verse_results = df_verses[df_verses['AyahKey'].str.startswith(query, na=False)].to_dict(orient='records')
-    elif starts_with_arabic(query):
-        verse_results = df_verses[df_verses['ArabicText'].str.contains(query, na=False)].to_dict(orient='records')
-    else:
-        verse_results = df_verses[df_verses['EnglishTranslation'].str.contains(query, na=False)].to_dict(orient='records')
+    else:# starts_with_arabic(query):
+        verse_results = df_verses[df_verses['aya_text'].str.contains(query, na=False)].to_dict(orient='records')
+    # else:
+    #     verse_results = df_verses[df_verses['EnglishTranslation'].str.contains(query, na=False)].to_dict(
+    #         orient='records')
 
     results = verse_results
 
     return jsonify(results)
+
 
 @app.route('/get_annotations', methods=['GET'])
 def get_annotations():
@@ -42,14 +67,58 @@ def get_annotations():
 
     results = []
     for manuscript_id, annotations_list in annotations.items():
+        annotations_list = annotations_list.to_dict(orient='records')
         manuscript_annotations = [a for a in annotations_list if a['verse_id'] == query]
         results.append({
-            'manuscript_name': f"Manuscript_{manuscript_id}",
+            'manuscript_name': f"Manuscript {manuscript_id}",
             'manuscript_id': manuscript_id,
             'annotations': manuscript_annotations
         })
 
-    return jsonify(results)
+    return results
+
+
+@app.route('/get_manuscripts', methods=['GET'])
+def get_manuscripts():
+    results = []
+    for manuscript_id, _ in annotations.items():
+        results.append({
+            'manuscript_name': manuscript_id,
+            'manuscript_id': manuscript_id,
+        })
+
+    return results
+@app.route('/get_languages', methods=['GET'])
+def get_languages():
+    m = request.args.get("manuscript", "")
+    results = []
+    if m != "":
+        results = annotations[m]['annotation_Language'].unique().tolist()
+        # print(sorted(results))
+    return results
+
+@app.route('/get_annotation_types', methods=['GET'])
+def get_annotation_types():
+    m = request.args.get("manuscript", "")
+    results = []
+    if m != "":
+        results = annotations[m]['annotation_type'].unique().tolist()
+        # print(sorted(results))
+    return results
+
+
+@app.route('/save-annotation', methods=['POST'])
+def save_annotation():
+    data = request.json
+    # Process the annotation data here
+    # For example, save it to a database
+    manus_id = data["manuscript_id"]
+    data['annotation_id'] = f"{len(annotations[manus_id])}"
+    df_dictionary = pd.DataFrame([data])
+    annotations[manus_id] = pd.concat([annotations[manus_id], df_dictionary], ignore_index=True)
+    annotations[manus_id].to_excel('Dataset-Verse-by-Verse1-Konduga.xlsx', index=False)
+    return jsonify({"message": "Annotation saved successfully"}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,243 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Autocomplete from 'react-autocomplete';
 import './App.css';
 
-const ContextMenu= ({x, y, onClose, onAnnotate}) => {
+interface Verse {
+    AyahKey: string;
+    // old verse by verse
+    // SrNo: number;
+    // Juz: number;
+    // JuzNameArabic: string;
+    // JuzNameEnglish: string;
+    // SurahNo: number;
+    // SurahNameArabic: string;
+    // SurahNameEnglish: string;
+    // SurahMeaning: string;
+    // WebLink: string;
+    // Classification: string;
+    // AyahNo: number;
+    // EnglishTranslation: string;
+    // OrignalArabicText: string;
+    // ArabicText: string;
+    // ArabicWordCount: number;
+    // ArabicLetterCount: number;
+
+    // source : https://qurancomplex.gov.sa/en/techquran/dev/
+    id: string;
+    jozz: string;
+    page: string;
+    sura_no: string;
+    sura_name_en: string;
+    sura_name_ar: string;
+    line_start: string;
+    line_end: string;
+    aya_no: string;
+    aya_text: string;
+}
+
+interface Annotation {
+    annotation_id: string;
+    manuscript_id: string;
+    verse_id: string;
+    annotated_object: string;
+    annotated_range: string;
+    annotation: string;
+    annotation_Language: string;
+    annotation_transliteration: string;
+    annotation_type: string;
+    other: string;
+}
+
+interface AnnotationResult {
+    manuscript_name: string;
+    manuscript_id: string;
+    annotations: Annotation[];
+}
+
+interface ContextMenuProps {
+    x: number;
+    y: number;
+    onClose: () => void;
+    onAddAnnotationClicked: () => void;
+}
+
+interface VerseProps {
+    verse: Verse;
+    selectedManuscript: string;
+}
+
+interface PopupFormProps {
+    isOpen: boolean;
+    onClose: () => void;
+    initialVerseId: string;
+    initialAnnotatedObject: string;
+    initialSelectedRange: string;
+    selectedManuscript: string;
+}
+
+const PopupForm: React.FC<PopupFormProps> = ({
+                                                 isOpen,
+                                                 onClose,
+                                                 initialVerseId,
+                                                 initialAnnotatedObject,
+                                                 initialSelectedRange,
+                                                 selectedManuscript
+                                             }) => {
+
+    const [annotation, setAnnotation] = useState<Annotation>({
+        manuscript_id: selectedManuscript,
+        annotation_id: '',
+        verse_id: initialVerseId,
+        annotated_object: initialAnnotatedObject,
+        annotation: '',
+        annotation_Language: '',
+        annotation_transliteration: '',
+        annotated_range: initialSelectedRange,
+        annotation_type: '',
+        other: ''
+    });
+    const [languages, setLanguages] = useState<Array<string>>([]);
+    const [annotationTypes, setAnnotationTypes] = useState<Array<string>>([]);
+
+    useEffect(() => {
+        if (selectedManuscript !== "All Manuscripts") {
+            fetch();
+        }
+        setAnnotation(prevAnnotation => ({
+            ...prevAnnotation,
+            manuscript_id: selectedManuscript,
+            verse_id: initialVerseId,
+            annotated_object: initialAnnotatedObject,
+            annotated_range: initialSelectedRange
+        }));
+    }, [selectedManuscript, initialVerseId, initialAnnotatedObject, initialSelectedRange]);
+
+
+    const fetch = async () => {
+        try {
+            const response = await axios.get<Array<string>>(`http://localhost:5000/get_languages?manuscript=${selectedManuscript}`);
+            setLanguages(response.data);
+        } catch (error) {
+            alert('Error fetching languages: ' + error);
+        }
+        try {
+            const response = await axios.get<Array<string>>(`http://localhost:5000/get_annotation_types?manuscript=${selectedManuscript}`);
+            setAnnotationTypes(response.data);
+        } catch (error) {
+            alert('Error fetching annotation types: ' + error);
+        }
+    };
+
+
+    // const languages = ['Ar'];
+    // const annotationTypes = ['Translation'];
+
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setAnnotation((prevAnnotation) => ({
+            ...prevAnnotation,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('Form submitted:', annotation);
+
+        try {
+            const response = await axios.post('http://localhost:5000/save-annotation', annotation);
+            console.log('Server response:', response.data);
+            onClose();
+        } catch (error) {
+            alert('Error saving annotation:' + error);
+        }
+    };
+
+    if (!isOpen) return null;
+
+    const formFields = ['verse_id', 'manuscript_id', 'annotated_object', 'annotated_range', 'annotation', 'annotation_Language', 'annotation_transliteration', 'annotation_type', 'other'];
+    const handleAutocompleteChange = (field: keyof Annotation, value: string) => {
+        setAnnotation((prevAnnotation) => ({
+            ...prevAnnotation,
+            [field]: value,
+        }));
+    };
+
+    const renderInput = (key: keyof Annotation) => {
+        if (key === 'verse_id' || key === 'annotated_object' || key === 'annotated_range' || key === 'manuscript_id') {
+            return (
+                <input
+                    type="text"
+                    className="form-control"
+                    id={key}
+                    name={key}
+                    value={key === 'verse_id' ? initialVerseId : (key === 'annotated_object' ? initialAnnotatedObject : (key === 'annotated_range' ? initialSelectedRange : (key === 'manuscript_id' ? selectedManuscript : '')))}
+                    disabled
+                />
+            );
+        } else if (key === 'annotation_Language' || key === 'annotation_type') {
+            const items = key === 'annotation_Language' ? languages : annotationTypes;
+            return (
+                <Autocomplete
+                    value={annotation[key]}
+                    items={items}
+                    getItemValue={(item) => item}
+                    shouldItemRender={(item, value) => item.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                    renderItem={(item, isHighlighted) => (
+                        <div
+                            key={item}
+                            style={{background: isHighlighted ? 'lightgray' : 'white'}}
+                        >
+                            {item}
+                        </div>
+                    )}
+                    onChange={(e) => handleAutocompleteChange(key, e.target.value)}
+                    onSelect={(val) => handleAutocompleteChange(key, val)}
+                    inputProps={{
+                        className: 'form-control',
+                        id: key,
+                        name: key,
+                    }}
+                />
+            );
+        } else {
+            return (
+                <input
+                    type="text"
+                    className="form-control"
+                    id={key}
+                    name={key}
+                    value={annotation[key]}
+                    onChange={handleInputChange}
+                />
+            );
+        }
+    };
+
+    return (
+        <div className="popup">
+            <div className="popup-content">
+                <h2>Annotation Form</h2>
+                <form onSubmit={handleSubmit}>
+                    {formFields.map((key) => (
+                        <div key={key} className="form-group">
+                            <label htmlFor={key}>{key.replace('_', ' ')}:</label>
+                            {renderInput(key as keyof Annotation)}
+                        </div>
+                    ))}
+                    <div className="mt-3">
+                        <button type="submit" className="btn btn-primary mr-2">Submit</button>
+                        <button type="button" className="btn btn-secondary" onClick={() => onClose()}>
+                            Close
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+const ContextMenu: React.FC<ContextMenuProps> = ({x, y, onClose, onAddAnnotationClicked}) => {
     return (
         <div style={{
             position: 'fixed',
@@ -15,29 +251,48 @@ const ContextMenu= ({x, y, onClose, onAnnotate}) => {
             padding: '5px',
             zIndex: 1000
         }}>
-            <div onClick={() => {
+            <button onClick={() => {
+                console.log("clicked.1")
+                onAddAnnotationClicked()
             }}>
-                Add
-            </div>
+                Add Annotation
+            </button>
         </div>
     );
 };
 
 
-const Verse= ({verse}) => {
+const Verse: React.FC<VerseProps> = ({verse, selectedManuscript}) => {
     const [selectedText, setSelectedText] = useState<string>('');
+    const [selectedRange, setSelectedRange] = useState<string>('');
     const [contextMenu, setContextMenu] = useState<{ x: number; y: number } | null>(null);
-
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
     const containerRef = useRef<HTMLDivElement>(null);
 
-    const handleTextSelect = () => {
-        const selection = window.getSelection();
-        if (selection && selection.toString()) {
-            setSelectedText(selection.toString());
-        } else {
-            setSelectedText('');
-            setContextMenu(null);
+    const openPopup = () => setIsPopupOpen(true);
+    const closePopup = () => setIsPopupOpen(false);
+
+    const handleTextSelect = (event: React.MouseEvent) => {
+        // if (event.target === containerRef.current) {
+        if (!isPopupOpen) {
+            const selection = window.getSelection();
+            if (selection && selection.toString()) {
+                const range = selection.getRangeAt(0);
+                const preSelectionRange = range.cloneRange();
+                preSelectionRange.selectNodeContents(range.startContainer);
+                preSelectionRange.setEnd(range.startContainer, range.startOffset);
+
+                const start = preSelectionRange.toString().length;
+                const end = start + selectedText.length;
+
+                setSelectedText(selection.toString());
+                setSelectedRange(start + " - " + end)
+            } else {
+                // setSelectedText('');
+                setContextMenu(null);
+            }
         }
+        // }
     };
 
     const handleContextMenu = (event: React.MouseEvent) => {
@@ -47,8 +302,12 @@ const Verse= ({verse}) => {
         }
     };
 
-    const handleAddAnnotation = (annotation) => {
-        console.log('Annotation added for:', selectedText, 'Type:');
+    const handleAddAnnotationClicked = () => {
+        if (selectedManuscript === 'All Manuscripts') {
+            alert("Annotate mode does not work if no specific manuscript is selected!")
+        } else {
+            openPopup()
+        }
         setContextMenu(null);
     };
 
@@ -71,42 +330,79 @@ const Verse= ({verse}) => {
 
     return (<div className="alert alert-info" role="alert" ref={containerRef} onMouseUp={handleTextSelect}
                  onContextMenu={handleContextMenu}>
-        <h4 className="alert-heading">{verse.OrignalArabicText}</h4>
-        {/*<p>{this.props.verse.EnglishTranslation}</p>*/}
+        <PopupForm isOpen={isPopupOpen} onClose={closePopup} initialVerseId={verse.AyahKey}
+                   initialAnnotatedObject={selectedText} initialSelectedRange={selectedRange}
+                   selectedManuscript={selectedManuscript}/>
+        <h4 lang="ar" className="alert-heading">{verse.aya_text}</h4>
+        {/*<p>{verse.EnglishTranslation}</p>*/}
         {contextMenu && (
             <ContextMenu
                 x={contextMenu.x}
                 y={contextMenu.y}
                 onClose={handleCloseContextMenu}
-                onAnnotate={handleAddAnnotation}
+                onAddAnnotationClicked={handleAddAnnotationClicked}
             />
         )}
     </div>);
 };
 
-const App= () => {
+const App: React.FC = () => {
     const [query, setQuery] = useState<string>('');
-    const [selectedVerse, setSelectedVerse] = useState(null);
-    const [autocompleteResults, setAutocompleteResults] = useState([]);
-    const [annotationResults, setAnnotationResults] = useState([]);
+    const [selectedVerse, setSelectedVerse] = useState<Verse | null>(null);
+    const [selectedManuscript, setSelectedManuscript] = useState<string>('All Manuscripts');
+    const [autocompleteResults, setAutocompleteResults] = useState<Verse[]>([]);
+    const [annotationResults, setAnnotationResults] = useState<AnnotationResult[]>([]);
+    const [manuscripts, setManuscripts] = useState<Array<{ manuscript_id: string, manuscript_name: string }>>([]);
 
-    const handleSelectVerse = async (item) => {
+    useEffect(() => {
+        fetchManuscripts();
+    }, []);
+
+    const fetchManuscripts = async () => {
+        try {
+            const response = await axios.get<Array<{
+                manuscript_id: string,
+                manuscript_name: string
+            }>>('http://localhost:5000/get_manuscripts');
+            setManuscripts([{manuscript_id: 'all', manuscript_name: 'All Manuscripts'}, ...response.data]);
+        } catch (error) {
+            alert('Error fetching manuscripts: ' + error);
+            // Optionally, set some error state here to show to the user
+        }
+    };
+
+    const handleSelectVerse = async (item: Verse) => {
         setSelectedVerse(item);
-        setQuery(item.ArabicText);
+        setQuery(item.aya_text);
         setAutocompleteResults([]); // Clear the autocomplete results
-        const response = await axios.get(`http://localhost:5000/get_annotations?query=${item.AyahKey}`);
-        setAnnotationResults(response.data.map((item) => item));
+        try {
+            const response = await axios.get<AnnotationResult[]>(`http://localhost:5000/get_annotations?query=${item.AyahKey}`);
+            setAnnotationResults(response.data);
+        } catch (error) {
+            alert("Error : " + error)
+        }
     };
 
     const handleQueryChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
         setQuery(value);
         if (value.length >= 3) {
-            const response = await axios.get(`http://localhost:5000/search_verse?query=${value}`);
-            setAutocompleteResults(response.data.map((item) => item));
+            try {
+                const response = await axios.get<Verse[]>(`http://localhost:5000/search_verse?query=${value}`);
+                setAutocompleteResults(response.data);
+            } catch (error) {
+                alert("Error : " + error)
+            }
         } else {
             setAutocompleteResults([]);
         }
+    };
+
+    const handleManuscriptChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedManuscript(e.target.value);
+        // if (selectedVerse) {
+        //     handleSelectVerse(selectedVerse);
+        // }
     };
 
 
@@ -119,7 +415,7 @@ const App= () => {
                 <div className="input-group">
                     <Autocomplete
                         items={autocompleteResults}
-                        getItemValue={(item) => item.ArabicText}
+                        getItemValue={(item) => item.aya_text}
                         renderMenu={(items, value) => (
                             <div
                                 className="autocomplete-menu">{value.length > 0 && items.length === 0 ? `No matches for ${value}` : (items.length > 10 ? items.slice(0, 10) : items)}</div>
@@ -127,7 +423,7 @@ const App= () => {
                         renderItem={(item, isHighlighted) =>
                             <div className="autocomplete-item" key={item.AyahKey}
                                  style={{background: isHighlighted ? 'lightgray' : 'white'}}>
-                                {`${item.AyahKey}\t${item.ArabicText}`}
+                                {`${item.AyahKey}\t${item.aya_text}`}
                             </div>
                         }
                         value={query}
@@ -138,8 +434,27 @@ const App= () => {
                     />
                 </div>
             </form>
+            <div className="manuscript-selection mt-3">
+                {manuscripts.map((manuscript) => (
+                    <div key={manuscript.manuscript_id} className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="manuscriptRadio"
+                            id={`m-${manuscript.manuscript_id}`}
+                            value={manuscript.manuscript_name}
+                            checked={selectedManuscript === manuscript.manuscript_name}
+                            onChange={handleManuscriptChange}
+                        />
+                        <label className="form-check-label" htmlFor={`manuscript-${manuscript.manuscript_id}`}>
+                            {manuscript.manuscript_name}
+                        </label>
+                    </div>
+                ))}
+            </div>
+
             {selectedVerse && (
-                <Verse verse={selectedVerse}/>
+                <Verse verse={selectedVerse} selectedManuscript={selectedManuscript}/>
             )}
             <div className="accordion" id="resultsAccordion" style={{paddingBottom: '10rem'}}>
                 {annotationResults.map((result) => {
@@ -175,10 +490,16 @@ const App= () => {
                                                         className="list-group-item"
                                                         // onMouseOver={() => setSelectedVerse(result)}
                                                     >
-                                                        <strong>Annotation:</strong> {annotation.annotation}<br/>
                                                         <strong>Annotated
                                                             Object:</strong> {annotation.annotated_object}<br/>
-                                                        <strong>Type:</strong> {annotation.annotation_type}
+                                                        <strong>Annotation:</strong> {annotation.annotation}<br/>
+                                                        <strong>Annotation
+                                                            Range:</strong> {annotation.annotated_range}<br/>
+                                                        <strong>Annotation
+                                                            Language:</strong> {annotation.annotation_Language}<br/>
+                                                        <strong>Annotation
+                                                            Transliteration:</strong> {annotation.annotation_transliteration}<br/>
+                                                        <strong>Other Notes:</strong> {annotation.other}<br/>
                                                     </li>
                                                 ))}
                                             </ul>
